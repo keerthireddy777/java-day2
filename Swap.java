7bea50e950055cd25da51c60f331cfc04dead817
keerthireddy777 <vontikondakeerthireddy@gmail.com>
2026-02-07 15:19:11 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args){
+        int a=3;
+        int b=2;
+        a=a+b;
+        b=a-b;
+        a=a+b;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
