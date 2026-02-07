5c9f32d6f578e37ebc4155b5ecca5106e006b65f
keerthireddy777 <vontikondakeerthireddy@gmail.com>
2026-02-07 11:39:35 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add {
+    public static void main(String[] args){
+        int sub1=87;
+        int sub2=97;
+        int sub3=93;
+        int sum=(sub1+sub2+sub3);
+        System.out.println(sum);
+        
+
+    }
+}
