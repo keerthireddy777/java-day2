dab06198ff384a6313b94367d7bf67381ada544d
keerthireddy777 <vontikondakeerthireddy@gmail.com>
2026-02-07 11:25:53 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,23 @@
+public class Student1 {
+    public static void main(String[] args){
+        String name="keerthi";
+        int age=18;
+        char grade='A';
+        int marks=76;
+        String status;
+        if(marks>=35){
+            status ="pass";
+
+        }else{
+            status ="fail";
+
+        }
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Grade:"+grade);
+        System.out.println("Marks:"+marks);
+        System.out.println("Status:"+status);
+    
+        
+    }
+}
