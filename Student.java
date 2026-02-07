90427689a1ea90cba99c6a48c2e209dce2a046f1
keerthireddy777 <vontikondakeerthireddy@gmail.com>
2026-02-07 11:02:54 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public static void main(String[] args){
+        String name="keerthi";
+        int age=19;
+        float marks=20.6f;
+        boolean Standard=true;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(Standard);
+    }
+    
+}
